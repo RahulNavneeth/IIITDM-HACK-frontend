5756c2f0febf718ad0e1be6baaa8d4bb876d935e
RahulNavneeth <rahulmnavneeth@gmail.com>
2024-03-17 12:50:29 +0530

    build?

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,17 @@
+"use client"
+
+import { useEffect } from "react";
+
 export default function Home() {
+    useEffect(() => {
+        if ("d_token" in localStorage) {
+            window.location.href = "/info";
+        } else if ("h_token" in localStorage) {
+            window.location.href = "/hospital";
+        } else if ("p_token" in localStorage) {
+            window.location.href = "/info/cp";
+        }
+    }, [])
     return (
         <div className="w-full h-full flex flex-col items-center justify-center"> Loading... Please wait!!! </div>
     );
